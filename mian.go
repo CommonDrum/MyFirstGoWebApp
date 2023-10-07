549f89c6b80618fe87b27a944f9ef2c275bb094f
CommonDrum <karol.hetman@protonmail.com>
2023-10-07 14:31:34 +0100

    Repeat functionality

--- mian.go
+++ mian.go
@@ -4,8 +4,18 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World!")
+		c.HTML(200, "index.html", nil)
 	})
+
+	r.POST("/repeat", func(c *gin.Context) {
+		message := c.PostForm("message")
+		c.HTML(200, "index.html", gin.H{
+			"Message": message,
+		})
+	})
+
 	r.Run() // listens on port 8080 by default
 }
