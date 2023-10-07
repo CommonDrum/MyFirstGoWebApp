654077ac3df9e350ace9bb2dbc55b385d15eee23
CommonDrum <karol.hetman@protonmail.com>
2023-10-07 17:33:34 +0100

    experimenting

--- mian.go
+++ mian.go
@@ -12,9 +12,13 @@ func main() {
 
 	r.POST("/repeat", func(c *gin.Context) {
 		message := c.PostForm("message")
-		c.HTML(200, "index.html", gin.H{
-			"Message": message,
+		c.HTML(200, "repeat.html", gin.H{
+			"message": message,
 		})
+	})
+
+	r.GET("/next", func(c *gin.Context) {
+		c.HTML(200, "next_page.html", nil)
 	})
 
 	r.Run() // listens on port 8080 by default
